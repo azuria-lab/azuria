a5936d457a28fccea746702ae888f56dc5c05568
Azuria <azuria.labs@gmail.com>
2025-11-20 15:44:51 -0300

    feat: adicionar rotas de licitacao no app.tsx
    Rotas: /calculadora-licitacao, /dashboard-licitacoes, /documentos

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-
+﻿
 import { lazy, Suspense } from "react";
 import { Toaster } from "@/components/ui/toaster";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -156,3 +156,4 @@ const App = () => {
 };
 
 export default App;
+
